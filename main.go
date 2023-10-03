1124e6a266ff0dae98107bdc7bec1d7f183276f6
小熊 <627516430@qq.com>
2023-10-03 19:22:23 +0800

    使用bee api 初始化项目

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/xiaoxiongmao5/xoj/xoj-code-sandbox/routers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
